86224864aaf80fb08b04f4bfd2040f6be8a2c2c6
Kenzar <101153511+Kenzar-San@users.noreply.github.com>
2022-03-20 22:25:17 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,32 @@
+const listNumbers = document.querySelectorAll('[data-value]')
+const sections = document.querySelectorAll('.section')
+const submit = document.querySelector('.submit')
+const score = document.querySelector('.score')
+
+let value = 0
+
+listNumbers.forEach(rating => {
+
+    rating.addEventListener('click', () => {
+
+        listNumbers.forEach(rating => rating.classList.remove("active"))
+        
+        value = rating.dataset.value
+        
+        rating.classList.add("active")
+        console.log(rating)
+    })
+})
+
+        submit.addEventListener("click", () => {
+            if(value!=0){
+                // score.textContent = "value"
+                const openContainer = document.querySelector('.open')
+                openContainer.classList.remove('open')
+                const toOpenContainer = document.querySelector('.thanks')
+                toOpenContainer.classList.add('open')
+                score.classList.add('score')
+            
+        } 
+    })
+        
